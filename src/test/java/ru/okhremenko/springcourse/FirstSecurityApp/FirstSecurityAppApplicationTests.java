d43959d3d564063b7a10d8e758b53365d157651f
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-11-13 17:47:58 +0300

    initial commit

---
+++ src/test/java/ru/okhremenko/springcourse/FirstSecurityApp/FirstSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.okhremenko.springcourse.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
