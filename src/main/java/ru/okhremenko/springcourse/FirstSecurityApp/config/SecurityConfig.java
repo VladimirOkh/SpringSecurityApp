4263865f2e593beb29d73ea52127a922133dd380
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-11-18 00:02:43 +0300

    add custom auth page

--- src/main/java/ru/okhremenko/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/okhremenko/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package ru.okhremenko.springcourse.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
     }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //Конфигурируем сам Spring Security
+        //Конфигурируем авторизацию
+        http.csrf().disable() //отключаем защиту от межсайтовой подделки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated() //Авторизация
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true) //В случае успешной аутентификации перенаправляем
+                .failureUrl("/auth/login?error");
+    }
 
+    //Настраиваем аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
