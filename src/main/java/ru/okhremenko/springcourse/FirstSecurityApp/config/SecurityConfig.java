a6ee185c54dc68a22b0e29c638f28ed629f22589
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2024-01-20 00:04:33 +0300

    add validation and registration

--- src/main/java/ru/okhremenko/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/okhremenko/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //Конфигурируем авторизацию
         http.csrf().disable() //отключаем защиту от межсайтовой подделки запросов
                 .authorizeRequests()
-                .antMatchers("/auth/login", "/error").permitAll()
+                .antMatchers("/auth/login","/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated() //Авторизация
                 .and()
                 .formLogin().loginPage("/auth/login")
---
+++ src/main/java/ru/okhremenko/springcourse/FirstSecurityApp/services/RegistrationService.java
@@ -0,0 +1,20 @@
+package ru.okhremenko.springcourse.FirstSecurityApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.okhremenko.springcourse.FirstSecurityApp.models.Person;
+import ru.okhremenko.springcourse.FirstSecurityApp.repositories.PeopleRepository;
+@Service
+public class RegistrationService {
+
+    private final PeopleRepository peopleRepository;
+    @Autowired
+    public RegistrationService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+    @Transactional
+    public void register(Person person) {
+        peopleRepository.save(person);
+    }
+}
